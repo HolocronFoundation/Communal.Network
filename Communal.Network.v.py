93f6e7c11c907febcc484b2ea7018d16c7324090
SamuelTroper <samuel.troper@holocron.foundation>
2019-03-23 12:28:02 -0400

    Bits on bits

--- Communal.Network.v.py
+++ Communal.Network.v.py
@@ -1,3 +1,5 @@
+# [[To do - remove sendMessage method for optimization purposes]]
+
 message: event({messageIndex: indexed(uint256), replyToIndex: indexed(uint256), messageInfo: indexed(uint256)})
 # messageIndex is the index of the sent message in C.N.
 # messageInfo contains, from most significant to least significant bits/bytes:
@@ -8,14 +10,15 @@ lastMessageNumber: public(uint256)
 externalSenderAuthorization: bytes[12][address][address]
 # address is the authorizing user
 # bytes[12] contains, from most significant to least significant bits/bytes:
-#       - 1 bit: blanket authorization
+#       - 1 bit: authorization
 #       - 1 bit: limited message count authorization
 #       - 2 bytes: limited message count authorization - remaining messages
 
 authorizationBit: constant(uint256) = shift(1, 95)
 authorizationBit_inverted: constant(uint256) = bitwise_not(authorizationBit)
 authorizationBit_limitedMessages: constant(uint256) = shift(1, 94)
-limitedMessageAuthorization_usedBits: constant(uint256) = shift(2**16, 78)
+limitedMessageAuthorization_usedBitsMask: constant(uint256) = shift(2**16, 78)
+limitedMessageAuthorization_usedBitsMask_inverted: constant(uint256) = bitwise_not(limitedMessageAuthorization_usedBitsMask)
 
 @private
 def sendMessage(sender: address, replyToIndex: uint256, messageInfo: uint256):
@@ -30,18 +33,22 @@ def sendMessageUser(message: bytes[MAX_UINT256], replyToIndex: uint256 = 0, mess
 
 @public
 @payable
-def sendMessageExternalUser(_sender: address, messageString: bytes[MAX_UINT256], replyToIndex: uint256 = 0, messageInfo: uint256 = 0):
-        assert bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.blanketAuthorizationBit) == self.blanketAuthorizationBit \
-               or \
-               (bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.authorizationBit_limitedMessages) == self.authorizationBit_limitedMessages and  \
-                shift(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.limitedMessageAuthorization_usedBits), -78) > 0)     
+def sendMessageExternalUser(_sender: address, message: bytes[MAX_UINT256], replyToIndex: uint256 = 0, messageInfo: uint256 = 0):
+        assert bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.authorizationBit) == self.authorizationBit
+        if bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.authorizationBit_limitedMessages) == self.authorizationBit_limitedMessages:
+                assert shift(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.limitedMessageAuthorization_usedBits), -78) > 0
+                self.externalSenderAuthorization[msg.sender][_sender] = bitwise_or(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], \
+                                                                                               self.limitedMessageAuthorization_usedBitsMask_inverted), \
+                                                                                   bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], \
+                                                                                               self.limitedMessageAuthorization_usedBitsMask) \
+                                                                                   - shift(1, 78))
         self.sendMessage(_sender, replyToIndex, messageInfo)
 
 ###   External sender functionality   ###
 
 @public
 def authorizeSender_blanket(sender: address):
-        self.externalSenderAuthorization[sender][msg.sender] = bitwise_or(self.externalSenderAuthorization[msg.sender], self.blanketAuthorizationBit)
+        self.externalSenderAuthorization[sender][msg.sender] = bitwise_or(self.externalSenderAuthorization[msg.sender], self.authorizationBit)
 
 @public
 def deauthorizeSender_blanket(sender: address):
