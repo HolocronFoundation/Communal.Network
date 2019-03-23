23642f7906250bd13ae4cf027a1a7edb9d2926a3
SamuelTroper <samuel.troper@holocron.foundation>
2019-03-23 10:13:09 -0400

    Updates

--- Communal.Network.v.py
+++ Communal.Network.v.py
@@ -1,23 +1,24 @@
-message: event({messageIndex: indexed(uint256), sender: indexed(address), messageContent: bytes[32]})
+message: event({messageIndex: indexed(uint256), sender: indexed(address), replyToIndex: indexed(uint256), metadataCode: uint256, messageContent: bytes[128]})
 
 lastMessageNumber: public(uint256)
 externalSenderAuthorization: bool[address][address] # bool - T if authorized external sender | address - external address | address - user address
 
 @private
-def sendMessage(sender: address, messageContent: bytes[32]):
-        log.message(self.lastMessageNumber, sender, messageContent)
+def sendMessage(sender: address, replyToIndex: uint256, messageContent: bytes[32], metadataCode: uint256):
+        assert replyToIndex <= self.lastMessageNumber
+        log.message(self.lastMessageNumber, sender, replyToIndex, metadataCode, messageContent)
         self.lastMessageNumber += 1
 
 @public
 @payable
-def sendMessageUser(messageString: bytes[32]):
-        self.sendMessage(msg.sender, messageString)
+def sendMessageUser(messageString: bytes[32], replyToIndex: uint256 = 0, metadataCode: uint256 = 0):
+        self.sendMessage(msg.sender, replyToIndex, messageString, metadataCode)
 
 @public
 @payable
-def sendMessageExternalUser(_sender: address, messageString: bytes[32]):
+def sendMessageExternalUser(_sender: address, messageString: bytes[32], replyToIndex: uint256 = 0, metadataCode: uint256 = 0):
         assert self.externalSenderAuthorization[msg.sender][_sender]
-        self.sendMessage(_sender, messageString)
+        self.sendMessage(_sender, replyToIndex, messageString, metadataCode)
 
 ###
 
