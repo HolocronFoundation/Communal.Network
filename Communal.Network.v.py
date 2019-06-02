819026678c82c042b97215cdbaad88827d1b5afd
Samuel Troper <samuel.troper@holocron.fondation>
2019-06-02 17:01:00 -0400

    FInished reworking the contract

--- Communal.Network.v.py
+++ Communal.Network.v.py
@@ -1,81 +1,184 @@
-message: event({messageIndex: indexed(uint256), replyToIndex: indexed(uint256), messageInfo: indexed(uint256)})
-# messageIndex is the index of the sent message in C.N.
-# messageInfo contains, from most significant to least significant bits/bytes:
-#       - 20 bytes: the sender address
-#       - 1 bit: a flag to indicate the sender was external
-# [[Double check!!!]]
-messageInfoMask: constant(uint256) = bitwise_not(shift(2**161, 94))
+struct item_struct:
+    reply_to_index: uint256
+    item: bytes32
+    metadata: uint256
 
-lastMessageNumber: public(uint256)
+item: event({item_index: indexed(uint256), reply_to_index: indexed(uint256), metadata: indexed(uint256)})
+# item_index is the index of the sent item in C.N.
 
-externalSenderAuthorization: bytes[12][address][address]
-# address is the authorizing user
-# bytes[12] contains, from most significant to least significant bits/bytes:
-#       - 1 bit: authorization
-#       - 1 bit: limited message count authorization
-#       - 2 bytes: limited message count authorization - remaining messages
+last_item_index: public(uint256)
 
-# [[DOUBLE CHECK ALL THIS SHIT]]
-authorizationBit: constant(uint256) = shift(1, 95)
-authorizationBit_inverted: constant(uint256) = bitwise_not(authorizationBit)
-authorizationBit_limitedMessages: constant(uint256) = shift(1, 94)
-authorizationBit_limitedMessages_inverted: constant(uint256) = bitwise_not(authorizationBit_limitedMessages)
-limitedMessageAuthorization_usedBitsMask: constant(uint256) = shift(2**16, 78)
-limitedMessageAuthorization_usedBitsMask_inverted: constant(uint256) = bitwise_not(limitedMessageAuthorization_usedBitsMask)
+items: public(map(uint256, item_struct))
 
-lastMessageNumber: public(uint256)
-        
+# External sender mappings
+external_sender_authorization: public(map(address, map(address, bool)))
+# outer address is the authorizing user
 
-@public
-@payable
-def sendMessageUser(message: bytes[MAX_UINT256], replyToIndex: uint256 = 0, messageInfo: uint256 = 0):
-        assert replyToIndex <= self.lastMessageNumber
-        self.sendMessage(msg.sender, replyToIndex, messageInfo)
-        log.message(self.lastMessageNumber, sender, replyToIndex, prepMessageInfo(sender, False, messageInfo))
-        self.lastMessageNumber += 1 
+owner: constant(address) = 0xd015FB4e0c7f2D0592fa642189a70ce46C90d612
+custom_metadata_mask: constant(uint256) = 2 ** (12*8+1) - 1 - 7 # Fills 12 bytes with F, then sets the last three bits to 0
 
-@public
-@payable
-def sendMessageExternalUser(_sender: address, message: bytes[MAX_UINT256], replyToIndex: uint256 = 0, messageInfo: uint256 = 0):
-        assert replyToIndex <= self.lastMessageNumber
-        assert bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.authorizationBit) == self.authorizationBit
-        if bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.authorizationBit_limitedMessages) == self.authorizationBit_limitedMessages:
-                assert shift(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], self.limitedMessageAuthorization_usedBits), -78) > 0
-                self.externalSenderAuthorization[msg.sender][_sender] = bitwise_or(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], \
-                                                                                               self.limitedMessageAuthorization_usedBitsMask_inverted), \
-                                                                                   bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], \
-                                                                                               self.limitedMessageAuthorization_usedBitsMask) \
-                                                                                   - shift(1, 78))
-        self.sendMessage(_sender, replyToIndex, messageInfo)
-        log.message(self.lastMessageNumber, sender, replyToIndex, prepMessageInfo(sender, True, messageInfo))
-        self.lastMessageNumber += 1 
+# Helper methods - used across several disparate paths
 
+@private
 @constant
-def prepMessageInfo(sender: address, externalSender: bool, extraInfo: uint256):
-        return shift(convert(sender, uint256), 96) + shift(convert(externalSender, uint256), 95) + bitwise_and(extraInfo, messageInfoMask)
-        
+def external_sender_asserts(msg_sender: address, sender: address):
+    assert self.external_sender_authorization[msg_sender][sender]
+@private
+@constant
+def prep_custom_metadata(custom_metadata: uint256) -> uint256:
+    return bitwise_and(custom_metadata, custom_metadata_mask)
+
+@private
+def check_item_then_iterate_last_item(reply_to_index: uint256):
+    assert reply_to_index <= self.last_item_index
+    self.last_item_index += 1
+@private
+@constant
+def generate_metadata(sender: address, other_metadata: uint256) -> uint256:
+    # Default_metadata is 3 bits.
+    #   The leftmost bit represents if a message is a light item
+    #   The middle bit represents if a message is sent by an external user
+    #   The right bit represents if an item is an IPFS hash of form Qm (sha2-256)
+    #       Should this change in the future or you want to do something
+    #       different now, you should trigger this flag, but also use custom
+    #       metadata to indicate this difference.
+    return shift(convert(sender, uint256), 96) + other_metadata
+
+# Functions which feed into check_item_then_iterate_last_item, log.item, and generate_metadata
+@private
+def send_light_item(reply_to_index: uint256, sender: address, default_metadata: uint256):
+    self.check_item_then_iterate_last_item(reply_to_index)
+    log.item(self.last_item_index, reply_to_index, self.generate_metadata(sender, default_metadata + 4))
+@private
+def send_full_item(item: bytes32, reply_to_index: uint256, sender: address, default_metadata: uint256):
+    self.check_item_then_iterate_last_item(reply_to_index)
+    self.items[self.last_item_index].metadata = self.generate_metadata(sender, default_metadata)
+    self.items[self.last_item_index].reply_to_index = reply_to_index
+    self.items[self.last_item_index].item = item
+    log.item(self.last_item_index, reply_to_index, self.items[self.last_item_index].metadata)
+
+# Functions which feed into send_light_item
+@private
+def send_light_item_external(reply_to_index: uint256, sender: address, msg_sender: address, metadata: uint256):
+    self.external_sender_asserts(msg_sender, sender)
+    self.send_light_item(reply_to_index, sender, metadata + 2)
+@public
+@payable
+def send_light_message_user(message: bytes32, reply_to_index: uint256 = 0):
+    self.send_light_item(reply_to_index, msg.sender, 0)
+@public
+@payable
+def send_light_hash_user(hash: bytes32, reply_to_index: uint256 = 0):
+    self.send_light_item(reply_to_index, msg.sender, 1)
+@public
+@payable
+def send_light_message_user_with_metadata(message: bytes32, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_light_item(reply_to_index, msg.sender, self.prep_custom_metadata(custom_metadata))
+@public
+@payable
+def send_light_hash_user_with_metadata(hash: bytes32, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_light_item(reply_to_index, msg.sender, self.prep_custom_metadata(custom_metadata) + 1)
+
+# Functions which feed into send_full_item
+@private
+def send_full_item_external(item: bytes32, reply_to_index: uint256, sender: address, msg_sender: address, metadata: uint256):
+    self.external_sender_asserts(msg_sender, sender)
+    self.send_full_item(item, reply_to_index, sender, metadata + 2)
+@public
+@payable
+def send_full_message_user(message: bytes32, reply_to_index: uint256 = 0):
+    self.send_full_item(message, reply_to_index, msg.sender, 0)
+@public
+@payable
+def send_full_hash_user(hash: bytes32, reply_to_index: uint256 = 0):
+    self.send_full_item(hash, reply_to_index, msg.sender, 1)
+@public
+@payable
+def send_full_message_user_with_metadata(message: bytes32, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_full_item(message, reply_to_index, msg.sender, self.prep_custom_metadata(custom_metadata))
+@public
+@payable
+def send_full_hash_user_with_metadata(hash: bytes32, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_full_item(hash, reply_to_index, msg.sender, self.prep_custom_metadata(custom_metadata) + 1)
+
+# Functions which feed into send_full_item_external
+@public
+@payable
+def send_full_message_external(message: bytes32, sender: address, reply_to_index: uint256 = 0):
+    self.send_full_item_external(message, reply_to_index, sender, msg.sender, 0)
+@public
+@payable
+def send_full_hash_external(hash: bytes32, sender: address, reply_to_index: uint256 = 0):
+    self.send_full_item_external(hash, reply_to_index, sender, msg.sender, 1)
+@public
+@payable
+def send_full_message_external_with_metadata(message: bytes32, sender: address, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_full_item_external(message, reply_to_index, sender, msg.sender, self.prep_custom_metadata(custom_metadata))
+@public
+@payable
+def send_full_hash_external_with_metadata(hash: bytes32, sender: address, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_full_item_external(hash, reply_to_index, sender, msg.sender, self.prep_custom_metadata(custom_metadata) + 1)
+
+# Functions which feed into send_light_item_external
+@public
+@payable
+def send_light_message_external(message: bytes32, sender: address, reply_to_index: uint256 = 0):
+    self.send_light_item_external(reply_to_index, sender, msg.sender, 0)
+@public
+@payable
+def send_light_hash_external(hash: bytes32, sender: address, reply_to_index: uint256 = 0):
+    self.send_light_item_external(reply_to_index, sender, msg.sender, 1)
+@public
+@payable
+def send_light_message_external_with_metadata(message: bytes32, sender: address, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_light_item_external(reply_to_index, sender, msg.sender, self.prep_custom_metadata(custom_metadata))
+@public
+@payable
+def send_light_hash_external_with_metadata(hash: bytes32, sender: address, custom_metadata: uint256, reply_to_index: uint256 = 0):
+    self.send_light_item_external(reply_to_index, sender, msg.sender, self.prep_custom_metadata(custom_metadata) + 1)
+
+@private
+def edit(new_item: bytes32, item_index: uint256, sender: address):
+    assert shift(self.items[item_index].metadata, -96) == convert(sender, uint256)
+    self.items[item_index].item = new_item
+
+@private
+def edit_with_metadata(new_item: bytes32, item_index: uint256, sender: address, new_metadata: uint256):
+    self.edit(new_item, item_index, sender)
+    self.items[item_index].metadata = self.generate_metadata(sender, new_metadata)
+
+# Editing functions (full items only!)
+@public
+@payable
+def edit_full_item_user_with_metadata(new_item: bytes32, item_index: uint256, new_metadata: uint256, is_hash: bool = False):
+    self.edit_with_metadata(new_item, item_index, msg.sender, self.prep_custom_metadata(new_metadata) + convert(is_hash, uint256))
+@public
+@payable
+def edit_full_item_external_with_metadata(new_item: bytes32, item_index: uint256, sender: address, new_metadata: uint256, is_hash: bool = False):
+    self.external_sender_asserts(msg.sender, sender)
+    self.edit_with_metadata(new_item, item_index, sender, self.prep_custom_metadata(new_metadata) + 2 + convert(is_hash, uint256))
+@public
+@payable
+def edit_full_item_user(new_item: bytes32, item_index: uint256):
+    self.edit(new_item, item_index, msg.sender)
+@public
+@payable
+def edit_full_item_external(new_item: bytes32, item_index: uint256, sender: address):
+    self.external_sender_asserts(msg.sender, sender)
+    self.edit(new_item, item_index, sender)
 
 ###   External sender functionality   ###
 
 @public
 def authorizeSender(sender: address):
-        self.externalSenderAuthorization[sender][msg.sender] = bitwise_or(self.externalSenderAuthorization[sender][msg.sender], self.authorizationBit)
+    self.external_sender_authorization[sender][msg.sender] = True
 
 @public
 def deauthorizeSender(sender: address):
-        self.externalSenderAuthorization[sender][msg.sender] = bitwise_and(self.externalSenderAuthorization[sender][msg.sender], self.authorizationBit_inverted)
+    self.external_sender_authorization[sender][msg.sender] = False
+
+###   Withdraw donations functionality   ###
 
 @public
-def limitSenderMessages(sender: address, limit: uint256):
-        self.externalSenderAuthorization[sender][msg.sender] = bitwise_or(bitwise_and(self.externalSenderAuthorization[msg.sender][_sender], \
-                                                                                      limitedMessageAuthorization_usedBitsMask_inverted), \
-                                                                          authorizationBit_limitedMessages + shift(limit,78))
-
-@public
-def delimitSenderMessages(sender: address):
-        self.externalSenderAuthorization[sender][msg.sender] = bitwise_and(self.externalSenderAuthorization[sender][msg.sender], self.authorizationBit_limitedMessages_inverted)
-
-@public
-def authorizeSenderAndLimitMessages(sender: address, limit: uint256):
-        self.authorizeSender(sender)
-        self.limitSenderMessages(sender, limit)
+def withdraw():
+    send(owner, self.balance)
