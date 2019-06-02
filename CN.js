926bf1b0e416f86947650181c5b3ec4ec9c39d1d
Samuel Troper <samuel.troper@holocron.fondation>
2019-06-02 18:32:22 -0400

    Updated JS and HTML

--- CN.js
+++ CN.js
@@ -6,6 +6,7 @@
 // TODO: Add send message JS
 //    Useful function - fromUtf8, toUtf8
 // TODO: Add reply to message JS
+// TODO: Sort out getPastEvents
 
 // Globals
   // web3
@@ -16,7 +17,7 @@
         provider: null,
         endpoint: null
       }
-    };
+    }; // TODO: Combine into cn
   // Ethereum network info
     eth_network = {
       cn_contract_address: null, // Loaded via get_eth_network
@@ -34,12 +35,11 @@
 
 
 // Setting up basic web3 enviroment
-  window.addEventListener('load', function() {
+  window.addEventListener('load', async function() {
     cn_web3 = setup_web3();
-    eth_network = get_eth_network(); // TODO: Catch the throw
-    cn = load_cn();
-    load_feed(); //TODO: explicitly pass objects
-    //TODO: Populate feed with old messages
+    eth_network = await get_eth_network(cn_web3); // TODO: Catch the throw
+    cn = load_cn(eth_network);
+    load_feed(cn);
     //TODO: Add a refresh thing when new messages come in
   });
 
@@ -59,17 +59,16 @@ function load_item_data(item_event_data) { //TODO: Pass needed params
   else {
     item_data = add_full_item_data(item_event_data, item_data);
   }
-  return item_data
+  return item_data;
 }
 function load_light_item_data(item_event_data, item_data) { //TODO: Add params
   // // TODO: load light item
   // Load metadata
   // Load tx
   //  Load message or hash from tx
-  return item_data
+  return item_data;
 }
 function load_full_item_data(item_even_data, item_data) {
-  // TODO: Load full message
   // Load item from contract
   // Load metadata from contract
 }
@@ -109,9 +108,10 @@ function setup_web3(fallback = {provider: "infura", endpoint: "mainnet.infura.io
 // Utility functions
   // Get the ABI for the main communal network
     function get_cn_abi(type, name = null) {
+      //TODO: Load ABI from file
       switch (type) {
         case "main":
-            return [{"name": "message", "inputs": [{"type": "uint256", "name": "messageIndex", "indexed": true}, {"type": "uint256", "name": "replyToIndex", "indexed": true}, {"type": "address", "name": "messageSender", "indexed": true}], "anonymous": false, "type": "event"}, {"name": "sendMessageUser", "outputs": [], "inputs": [{"type": "bytes", "name": "message"}], "constant": false, "payable": true, "type": "function"}, {"name": "sendMessageUser", "outputs": [], "inputs": [{"type": "bytes", "name": "message"}, {"type": "uint256", "name": "replyToIndex"}], "constant": false, "payable": true, "type": "function"}, {"name": "sendMessageUser", "outputs": [], "inputs": [{"type": "bytes", "name": "message"}, {"type": "uint256", "name": "replyToIndex"}, {"type": "uint256", "name": "messageInfo"}], "constant": false, "payable": true, "type": "function"}, {"name": "sendMessageExternalUser", "outputs": [], "inputs": [{"type": "address", "name": "_sender"}, {"type": "bytes", "name": "message"}], "constant": false, "payable": true, "type": "function"}, {"name": "sendMessageExternalUser", "outputs": [], "inputs": [{"type": "address", "name": "_sender"}, {"type": "bytes", "name": "message"}, {"type": "uint256", "name": "replyToIndex"}], "constant": false, "payable": true, "type": "function"}, {"name": "sendMessageExternalUser", "outputs": [], "inputs": [{"type": "address", "name": "_sender"}, {"type": "bytes", "name": "message"}, {"type": "uint256", "name": "replyToIndex"}, {"type": "uint256", "name": "messageInfo"}], "constant": false, "payable": true, "type": "function"}, {"name": "authorizeSender", "outputs": [], "inputs": [{"type": "address", "name": "sender"}], "constant": false, "payable": false, "type": "function", "gas": 35617}, {"name": "deauthorizeSender", "outputs": [], "inputs": [{"type": "address", "name": "sender"}], "constant": false, "payable": false, "type": "function", "gas": 20647}, {"name": "withdraw", "outputs": [], "inputs": [], "constant": false, "payable": false, "type": "function", "gas": 35593}, {"name": "lastMessageNumber", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 633}];
+            return [{"name": "item", "inputs": [{"type": "uint256", "name": "item_index", "indexed": true}, {"type": "uint256", "name": "reply_to_index", "indexed": true}, {"type": "uint256", "name": "metadata", "indexed": true}], "anonymous": false, "type": "event"}, {"name": "send_light_message_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_message_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_full_hash_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_message_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "message"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "send_light_hash_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "hash"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "custom_metadata"}, {"type": "uint256", "name": "reply_to_index"}], "constant": false, "payable": true, "type": "function"}, {"name": "edit_full_item_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}, {"type": "uint256", "name": "new_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "edit_full_item_user_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}, {"type": "uint256", "name": "new_metadata"}, {"type": "bool", "name": "is_hash"}], "constant": false, "payable": true, "type": "function"}, {"name": "edit_full_item_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "new_metadata"}], "constant": false, "payable": true, "type": "function"}, {"name": "edit_full_item_external_with_metadata", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}, {"type": "address", "name": "sender"}, {"type": "uint256", "name": "new_metadata"}, {"type": "bool", "name": "is_hash"}], "constant": false, "payable": true, "type": "function"}, {"name": "edit_full_item_user", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}], "constant": false, "payable": true, "type": "function", "gas": 37964}, {"name": "edit_full_item_external", "outputs": [], "inputs": [{"type": "bytes32", "name": "new_item"}, {"type": "uint256", "name": "item_index"}, {"type": "address", "name": "sender"}], "constant": false, "payable": true, "type": "function", "gas": 39055}, {"name": "authorizeSender", "outputs": [], "inputs": [{"type": "address", "name": "sender"}], "constant": false, "payable": false, "type": "function", "gas": 36457}, {"name": "deauthorizeSender", "outputs": [], "inputs": [{"type": "address", "name": "sender"}], "constant": false, "payable": false, "type": "function", "gas": 21487}, {"name": "withdraw", "outputs": [], "inputs": [], "constant": false, "payable": false, "type": "function", "gas": 36433}, {"name": "last_item_index", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 1473}, {"name": "items__reply_to_index", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [{"type": "uint256", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 1708}, {"name": "items__item", "outputs": [{"type": "bytes32", "name": "out"}], "inputs": [{"type": "uint256", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 1744}, {"name": "items__metadata", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [{"type": "uint256", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 1774}, {"name": "external_sender_authorization", "outputs": [{"type": "bool", "name": "out"}], "inputs": [{"type": "address", "name": "arg0"}, {"type": "address", "name": "arg1"}], "constant": true, "payable": false, "type": "function", "gas": 1925}];
         default:
           throw "Invalid type";
       }
@@ -121,8 +121,8 @@ function setup_web3(fallback = {provider: "infura", endpoint: "mainnet.infura.io
     function load_cn(eth_network = window.eth_network){
       abi = get_cn_abi("main");
       contract = new cn_web3.js.eth.Contract(abi, eth_network.cn_contract_address); // TODO: Consider options
-      live_subscription = contract.events.message(); // TODO: Consider options
-      old_messages = contract.getPastEvents("message"); // TODO: Consider options
+      live_subscription = contract.events.item(); // TODO: Consider options
+      old_messages = contract.getPastEvents("item"); // TODO: Consider options
       return {
         abi: abi,
         contract: contract,
@@ -135,24 +135,24 @@ function setup_web3(fallback = {provider: "infura", endpoint: "mainnet.infura.io
 
   // Checks the network being used, allowing for switching between networks
     function get_eth_network(cn_web3 = window.cn_web3) {
-      cn_web3.js.version.getNetwork((err, netId) => {
+      return cn_web3.js.eth.net.getId().then(function(netId){
         network_name = "unknown";
         cn_contract_address = null;
         switch (netId) {
-          case "1":
+          case 1:
             network_name = "mainnet";
             break;
-          case "2":
+          case 2:
             network_name = "morden";
             break;
-          case "3":
+          case 3:
             network_name = "ropsten";
             break;
-          case "4":
+          case 4:
             network_name = "rinkeby";
-            cn_contract_address = "0x1635c211354931b4e58CBc359020651BCe8a3621";
+            cn_contract_address = "0xFc8703e6BfAc77cDF25736005DcCE4a6c49d415D";
             break;
-          case "42":
+          case 42:
             network_name = "kovan";
             break;
         }
