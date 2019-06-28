1cf473c2cceebeedaeaebc9ea5e172b2daf13909
Samuel Troper <samuel.troper@holocron.foundation>
2019-06-28 19:38:16 -0400

    Updated channels

--- channels.v.py
+++ channels.v.py
@@ -1,15 +1,20 @@
 channeled: event({channel_index: indexed(uint256)})
+channel_name_display: event({channel_index: indexed(uint256), channel_name_display: indexed(bytes32)})
 
 last_opened_channel: public(uint256)
 
-channel_name_unique: public(map(uint256, bytes32))
-name_unique_used: public(map(bytes32, uint256))
-
-channel_name_display: public(map(uint256, bytes32)) # TODO: Replace this with event
+channel_index_to_channel_name_unique: public(map(uint256, bytes32))
+channel_name_unique_to_channel_index: public(map(bytes32, uint256))
 
 channel_owner: public(map(uint256, address))
 
-# TODO: Add channel item
+# TODO: Add channel item - think about how this should work - perhaps a helper contract which sends the item, then channels it. Need to make sent items return their index
+@public
+def channel_item()
+# TODO: This
+# Check that the item was sent by the sender - need to use external interface
+
+# TODO: Add channel metadata
 
 @public
 @constant
@@ -18,7 +23,7 @@ def check_owner(channel_index: uint256, owner: address):
 
 @public
 @payable
-def open_new_channel_owned(first_owner: address = msg.sender):
+def open_new_channel(first_owner: address = msg.sender):
     self.last_opened_channel += 1
     self.channel_owner[self.last_opened_channel] = first_owner
 
@@ -32,15 +37,21 @@ def change_channel_owner(channel_index: uint256, new_owner: address):
 
 @public
 @payable
-def change_channel_name_unique(channel_index: uint256, new_name: bytes32):
+def change_channel_index_to_channel_name_unique(channel_index: uint256, new_name: bytes32):
     self.check_owner(channel_index, msg.sender)
-    # TODO: This
+    assert self.channel_name_unique_to_channel_index(new_name) == 0
+    self.channel_name_unique_to_channel_index[self.channel_index_to_channel_name_unique[channel_index]] = 0
+    self.channel_index_to_channel_name_unique[channel_index] = new_name
+    self.channel_name_unique_to_channel_index[new_name] = channel_index
+    # TODO: Check this when less tired
 
 @public
 @payable
-def delete_channel_name_unique(channel_index: uint256):
+def delete_channel_index_to_channel_name_unique(channel_index: uint256):
     self.check_owner(channel_index, msg.sender)
-    # TODO: This
+    self.channel_name_unique_to_channel_index[self.channel_index_to_channel_name_unique[channel_index]] = 0
+    self.channel_index_to_channel_name_unique[channel_index] = EMPTY_BYTES32
+    # TODO: Check this when less tired
 
 @public
 @payable
